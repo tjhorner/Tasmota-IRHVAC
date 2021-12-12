2c844d33db7cbbdb3c9b0f6418917ea62121328b
TJ Horner <me@tjhorner.com>
2021-12-12 01:49:26 -0500

    python ...

--- custom_components/tasmota_irhvac/climate.py
+++ custom_components/tasmota_irhvac/climate.py
@@ -907,7 +907,7 @@ class TasmotaIrhvac(ClimateEntity, RestoreEntity):
     async def async_send_cmd(self, attr_update=False):
         if attr_update:
             await self.async_update_state_attrs()
-        await self.send_ir()
+        await self.hass.async_add_executor_job(self.send_ir)
         await self.async_update_ha_state()
 
     async def async_update_state_attrs(self):
@@ -972,10 +972,10 @@ class TasmotaIrhvac(ClimateEntity, RestoreEntity):
         elif preset_mode == PRESET_NONE and self._is_away:
             self._is_away = False
             self._target_temp = self._saved_target_temp
-        await self.send_ir()
+        await self.hass.async_add_executor_job(self.send_ir)
         await self.async_update_ha_state()
 
-    async def send_ir(self):
+    def send_ir(self):
         """Send the payload to tasmota mqtt topic."""
         fan_speed = self.fan_mode
         # tweak for some ELECTRA_AC devices
@@ -1017,4 +1017,6 @@ class TasmotaIrhvac(ClimateEntity, RestoreEntity):
         }
         payload = (json.dumps(payload_data))
         # Publish mqtt message
-        await mqtt.async_publish(self.hass, self.topic, payload)
+        asyncio.run_coroutine_threadsafe(
+            mqtt.async_publish(self.hass, self.topic, payload), self.hass.loop
+        ).result()
